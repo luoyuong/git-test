efcb3741d6226b77b725db26a5e625d04e42b249
Luoyu <Luoyu@atguigu.com>
2022-03-23 16:58:05 +0800

    second commit

---
+++ src/main/java/com/hy/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.hy.git;
+
+/**
+ * @author hy
+ * @create 2022-03-23 16:43
+ * @Description
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+    }
+}
